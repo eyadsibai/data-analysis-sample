511ed76113f71c8f0248767b02bcb39931f1e455
Eyad Sibai <eyad.alsibai@gmail.com>
2015-02-02 19:50:29 +0100

    Add requirements.txt file

--- analysis/utils.py
+++ analysis/utils.py
@@ -1,10 +1,16 @@
 """ Utilities methods used all over the project."""
-import random
+import scipy as sp
 
-def get_halo():
-    """Get a halo."""
-    halos = ['Halo 1',
-             'Halo: Spartan Assault',
-             'Halo Wars',
-             'Halo 2']
-    return random.choice(halos)
+
+def log_loss(predicted, actual):
+    """ Vectorized computation of log loss """
+
+    epsilon = 1e-15
+    predicted = sp.maximum(epsilon, predicted)
+    predicted = sp.minimum(1 - epsilon, predicted)
+
+    # compute log loss function (vectorized)
+    ll = sum(actual * sp.log(predicted) +
+             sp.subtract(1, actual) * sp.log(sp.subtract(1, predicted)))
+    ll = ll * -1.0 / len(actual)
+    return ll
